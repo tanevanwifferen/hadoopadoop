aa7444d0c4f22fe1127bd54f023e3377d6aa510d
Geert Derks <g.a.f.derks@student.tue.nl>
2015-12-10 21:01:30 +0100

    Update mapper-test.py

--- mapper-test.py
+++ mapper-test.py
@@ -4,15 +4,19 @@ import sys
 import csv
 from langdetect import detect
 
-def mapper():
-  reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for $
+reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for x in sys.stdin)
 
-  for line in reader:
-    words = line[4].strip() # get tweet text
-    if words != 'TEXT': # ignore csv header
-      try:
-        if detect(unicode(words, "utf8")) == 'en':
-          print words
-      except Exception:
-        pass # ignore tweets that don't contain actual words
-
+for line in reader:
+  # remove leading and trailing whitespace
+  line = line[4].strip() # get tweet text
+  # ingore header
+  if line != 'TEXT': # ignore csv header
+    try:
+      # check if tweet is written in English
+      if detect(unicode(line, "utf8")) == 'en':
+        # split the line into words
+        words = line.split()
+        for word in words:
+          print '%s\t%s' % (word, 1)
+    except Exception:
+      pass # ignore tweets that don't contain actual words
