a77eeb723c022bed3c07c52bd6e1bddb750383d8
Geert Derks <g.a.f.derks@student.tue.nl>
2015-12-10 23:04:35 +0100

    Update mapper-A1.py

--- A1/mapper-A1.py
+++ A1/mapper-A1.py
@@ -6,18 +6,20 @@ from langdetect import detect
 
 reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for x in sys.stdin)
 
+counter = 50000
+
 for line in reader:
+
   # remove leading and trailing whitespace
   line = line[4].strip() # get tweet text
   # ingore header
   if line != 'TEXT': # ignore csv header
     try:
-      # check if tweet is written in English
-      if detect(unicode(line, "utf8")) == 'en':
-        # split the line into words
-        words = line.split()
-        for word in words:
-          if word.startswith("#"):
-            print '%s\t%s' % (word, 1)
+      # split the line into words
+      words = line.split()
+      for word in words:
+        if word.startswith("#"):
+          print '%s\t%s' % (word, 1)
+          counter = counter - 1
     except Exception:
       pass # ignore tweets that don't contain actual words
