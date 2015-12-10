085e44082b70ea25be0752633891e8f44451579c
Geert Derks <g.a.f.derks@student.tue.nl>
2015-12-10 19:26:03 +0100

    Update mapper-test.py

--- mapper-test.py
+++ mapper-test.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+from langdetect import detect
 
 def mapper():
   reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for x in sys.stdin)
@@ -9,4 +10,8 @@ def mapper():
   for line in reader:
     words = line[4].strip() # get tweet text
     if words != 'TEXT': # ignore csv header
-      print words
+      try:
+        if detect(unicode(words, "utf8")) == 'en':
+          print words
+        except Exception:
+          pass # ignore tweets that don't contain actual words
