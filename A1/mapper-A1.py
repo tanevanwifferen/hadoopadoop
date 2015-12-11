7eb8815b8f4e29195edc32f587db225aaf223101
Geert Derks <g.a.f.derks@student.tue.nl>
2015-12-11 16:37:43 +0100

    mapper-A1 now supports time periods
    mapper needs to be called like /.mapper-A1.py 00 03
    where this means that the mapper will return all the hashtags between 00:00:00 and 03:00:00

--- A1/mapper-A1.py
+++ A1/mapper-A1.py
@@ -4,17 +4,28 @@ import sys
 import csv
 from langdetect import detect
 
-reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for x in sys.stdin)
+# get arguments
+start = sys.argv[1]+':00:00'
+end = sys.argv[2]+':00:00'
+
+reader = csv.reader(x.replace('\0','').replace('\n','').replace('\r','') for x $
 
 for line in reader:
 
   # remove leading and trailing whitespace
-  line = line[4].strip() # get tweet text
+  text = line[4].strip() # get tweet text
+  date = line[5].strip() # get tweet created
+
+  # skip if tweet does not fall within time range
+  if text != 'TEXT': # ignore csv header:
+    if date[11:] < start or date[11:] > end:
+      continue
+
   # ingore header
-  if line != 'TEXT': # ignore csv header
+  if text != 'TEXT': # ignore csv header
     try:
       # split the line into words
-      words = line.split()
+      words = text.split()
       for word in words:
         if word.startswith("#"):
           print '%s\t%s' % (word, 1)
